b22b27b23e1051f4c0216b4fb7f2f8aa8a6b2022
Fartunin6 <53219903+Fartunin6@users.noreply.github.com>
2020-12-30 18:37:42 +0300

    staging version

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const hostname = window.location.hostname;
-export const api = {
-  localhost: 'http://localhost:5000/api',
-  'react-pizzass.herokuapp.com': 'https://react-pizzass.herokuapp.com',
-};
-
-const apiBase = api[hostname];
-
-export default apiBase;
--- src/services/PizzaApi.js
+++ src/services/PizzaApi.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-import apiBase from '../config';
+
+const BASE_API = 'http://localhost:3001/pizzas';
 
 const API = {
   async getPizzas(category, sortBy) {
     const SORT_BY = `&_sort=${sortBy.type}&_order=${sortBy.order}`;
     const CATEGORY = `${category !== null ? `category=${category}` : ''}`;
 
-    const data = await axios.get(`${apiBase}'/pizzas'?${CATEGORY}&${SORT_BY}`);
+    const data = await axios.get(`${BASE_API}?${CATEGORY}&${SORT_BY}`);
     return data.data;
   },
 };
