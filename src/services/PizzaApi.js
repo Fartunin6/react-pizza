f9c83e839c27db083ca7a064f356b36a17320382
Fartunin6 <53219903+Fartunin6@users.noreply.github.com>
2020-12-31 12:01:22 +0300

    changed port

--- src/services/PizzaApi.js
+++ src/services/PizzaApi.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const BASE_API = 'http://localhost:3001/pizzas';
-
 const API = {
   async getPizzas(category, sortBy) {
     const SORT_BY = `&_sort=${sortBy.type}&_order=${sortBy.order}`;
     const CATEGORY = `${category !== null ? `category=${category}` : ''}`;
 
-    const data = await axios.get(`${BASE_API}?${CATEGORY}&${SORT_BY}`);
+    const data = await axios.get(`/pizzas?${CATEGORY}&${SORT_BY}`);
     return data.data;
   },
 };
